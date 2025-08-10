a79704f8c908c68f88b662aa2d802d2fad2be90b
Atik Hasan <atikhasan.inbox@gmail.com>
2025-08-10 17:09:54 +0600

    update 2

--- base.py
+++ base.py
@@ -1,29 +1,67 @@
-# user input 2 value and operator
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
+# food ordering system
+# first it will show a menu with available foods and their prices
+# user input which foods and drinks they will order
+# the total cost will be then displayed with 2% VAT 
+# and then prompt user to give tips or not
+# then will show the final cost including foods ordered (optional)
 
-try:
-    a = float(input("Enter the first value :"))
-    op = input("Enter the operator :")
-    b = float(input("Enter the second value :"))
+print("Welcome to Shantor Goriber Restaurant\n")
+print("Menu :\n")
+
+menu = ["0# Pizza = 2$" , "1# Burger = 3$" , "2# Chowmin = 4$" , "3# coke = 1$" , "4# Orange Juice = 1$\n" ]
+
+for i in menu :
+    print(i)
+
+print("To Choose your orders press corresponding numbers in pos :\n ")
+
+isconfirm = 0
+bill = 0
+order = []
+
+while(isconfirm == 0):
 
 
-    if(op=="+"):
-        print(add(a,b))
-    elif(op=="-"):
-        print(sub(a,b))
-    elif(op=="*"):
-        print(mul(a,b))
-    elif(op=="/"):
-        print(div(a,b))
+    pos = input("To finalize your order press y") 
+    
+
+    if(pos=="0"):
+        order.append("Pizza        = 2$")
+        bill += 2
+    elif(pos=="1"):
+        order.append("Burger       = 3$")
+        bill += 3
+    elif(pos=="2"):
+        order.append("Chowmin      = 4$")
+        bill += 4
+    elif(pos=="3"):
+        order.append("coke         = 1$")
+        bill += 1
+    elif(pos=="4"):
+        order.append("Orange Juice = 1$")
+        bill += 1
+    elif(pos=="y"):
+        isconfirm = 1
     else:
-        print("Error: Invalid operator")
-except ValueError:
-    print("You didn't enter the values correctly")
+        print("Invalid Input")
+        isconfirm = 1
 
+vat = bill*(2/100)
+
+print("Recipt :\n")
+for i in order:
+    print(i)
+
+print("\n")
+print("------------------------------")
+print(f"Subtotal = {bill}")
+print(f"VAT (2%) = {vat}")
+tips=0
+prompt=input("Would you like to leave us a tip (y/n)")
+if(prompt=="y"):
+    tips = int(input("Add the amount of tips"))
+    print(f"Tips     = {tips}")
+    print(f"Total    = {bill + tips + vat}\n")
+else:
+    print(f"Total    = {bill + tips + vat}\n")
+print("Thank you for your Purchase !")
